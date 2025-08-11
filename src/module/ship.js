287e42b88a3b6c4785250f2ff0465ab61c56f06d
kapindo <kapindo@gmail.com>
2025-08-12 00:28:34 +0700

    feat: Add Ship class with hit and sunk logic

---
+++ src/module/ship.js
@@ -0,0 +1,15 @@
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    if (this.hits === this.length) return null;
+    this.hits++;
+  }
+
+  isSunk() {
+    return this.hits === this.length;
+  }
+}
