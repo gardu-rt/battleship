38195eadbc60cbb7d5f74ba0ea9734868cddbb8d
kapindo <kapindo@gmail.com>
2025-08-12 01:37:27 +0700

    feat: Add Gameboard class with some logic: recieve attack ship placement

---
+++ src/module/gameboard.js
@@ -0,0 +1,71 @@
+import Ship from "./ship.js";
+
+export default class Gameboard {
+  constructor() {
+    this.size = 10;
+    this.board = Array.from({ length: this.size }, () =>
+      Array(this.size).fill(null)
+    );
+    this.ships = [];
+    this.attackedCoord = new Set();
+  }
+
+  placeShip(ship = new Ship(3), [row, col], horizontal) {
+    if (this.isOutBound([row, col], ship.length, horizontal)) {
+      throw new Error("invalid placement");
+    }
+    if (this.isOverlap([row, col], ship.length.horizontal)) {
+      throw new Error("invalid placement");
+    }
+
+    for (let i = 0; i < ship.length; i++) {
+      const r = horizontal ? row : row + i;
+      const c = horizontal ? col + i : col;
+      this.board[r][c] = ship;
+    }
+    this.ships.push(ship);
+  }
+
+  isOutBound([row, col], length, horizontal) {
+    for (let i = 0; i < length; i++) {
+      const r = horizontal ? row : row + i;
+      const c = horizontal ? col + i : col;
+
+      if (horizontal && c + length >= this.size) return true;
+      if (!horizontal && r + length >= this.size) return true;
+    }
+
+    return false;
+  }
+
+  isOverlap([row, col], length, horizontal) {
+    for (let i = 0; i < length; i++) {
+      const r = horizontal ? row : row + i;
+      const c = horizontal ? col + i : col;
+
+      if (this.board[r][c] !== null) return true;
+    }
+
+    return false;
+  }
+
+  recieveAttack([row, col]) {
+    if (this.attackedCoord.has([row, col].toString())) return;
+    if (this.isMiss([row, col])) {
+      this.attackedCoord.add([row, col].toString());
+      return "miss";
+    }
+    if (this.isHit([row, col])) {
+      this.attackedCoord.add([row, col].toString());
+      return "hit";
+    }
+  }
+
+  isMiss([row, col]) {
+    return this.board[row][col] === null;
+  }
+
+  isHit([row, col]) {
+    return this.board[row][col] !== null;
+  }
+}
