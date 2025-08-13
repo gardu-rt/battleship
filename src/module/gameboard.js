47d78b714954ddd4e708a7cb0561d07896b6e3b6
kapindo <kapindo@gmail.com>
2025-08-13 08:48:02 +0700

    refactor: change coordinate variable to x y

--- src/module/gameboard.js
+++ src/module/gameboard.js
@@ -10,26 +10,26 @@ export default class Gameboard {
     this.attackedCoord = new Set();
   }
 
-  placeShip(ship = new Ship(3), [row, col], horizontal) {
-    if (this.isOutBound([row, col], ship.length, horizontal)) {
+  placeShip(ship = new Ship(3), [x, y], horizontal) {
+    if (this.isOutBound([x, y], ship.length, horizontal)) {
       throw new Error("invalid placement");
     }
-    if (this.isOverlap([row, col], ship.length.horizontal)) {
+    if (this.isOverlap([x, y], ship.length.horizontal)) {
       throw new Error("invalid placement");
     }
 
     for (let i = 0; i < ship.length; i++) {
-      const r = horizontal ? row : row + i;
-      const c = horizontal ? col + i : col;
+      const r = horizontal ? x : x + i;
+      const c = horizontal ? y + i : y;
       this.board[r][c] = ship;
     }
     this.ships++;
   }
 
-  isOutBound([row, col], length, horizontal) {
+  isOutBound([x, y], length, horizontal) {
     for (let i = 0; i < length; i++) {
-      const r = horizontal ? row : row + i;
-      const c = horizontal ? col + i : col;
+      const r = horizontal ? x : x + i;
+      const c = horizontal ? y + i : y;
 
       if (horizontal && c + length >= this.size) return true;
       if (!horizontal && r + length >= this.size) return true;
@@ -38,10 +38,10 @@ export default class Gameboard {
     return false;
   }
 
-  isOverlap([row, col], length, horizontal) {
+  isOverlap([x, y], length, horizontal) {
     for (let i = 0; i < length; i++) {
-      const r = horizontal ? row : row + i;
-      const c = horizontal ? col + i : col;
+      const r = horizontal ? x : x + i;
+      const c = horizontal ? y + i : y;
 
       if (this.board[r][c] !== null) return true;
     }
@@ -49,24 +49,24 @@ export default class Gameboard {
     return false;
   }
 
-  recieveAttack([row, col]) {
-    if (this.attackedCoord.has([row, col].toString())) return null;
-    if (this.isMiss([row, col])) {
-      this.attackedCoord.add([row, col].toString());
+  recieveAttack([x, y]) {
+    if (this.attackedCoord.has([x, y].toString())) return null;
+    if (this.isMiss([x, y])) {
+      this.attackedCoord.add([x, y].toString());
       return "miss";
     }
-    if (this.isHit([row, col])) {
-      this.attackedCoord.add([row, col].toString());
+    if (this.isHit([x, y])) {
+      this.attackedCoord.add([x, y].toString());
       return "hit";
     }
   }
 
-  isMiss([row, col]) {
-    return this.board[row][col] === null;
+  isMiss([x, y]) {
+    return this.board[x][y] === null;
   }
 
-  isHit([row, col]) {
-    return this.board[row][col] !== null;
+  isHit([x, y]) {
+    return this.board[x][y] !== null;
   }
 
   allShipSunk() {
